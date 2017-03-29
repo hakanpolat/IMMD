5a9ee17bc9a0fbda2f57d2fa7049f0b308bf78b4
mesutto <mesutugur@gmail.com>
2017-03-29 21:23:13 +0300

    no message

--- IMMD BAP/design/motor_parameters.m
+++ IMMD BAP/design/motor_parameters.m
@@ -2,7 +2,7 @@
 
 % Datasheet parameters
 % KBM(S)-45X02-X - B
-pole = 10;
+rotor_pole = 10;
 Prated = 7200; % W
 Tstall_cont = 43.5; % Nm
 Tstall_peak = 171; % Nm
@@ -14,8 +14,40 @@ Rlinetoline = 0.76; %Ohms
 Lm = 12; % mH
 Vt_ll = 480; % V
 
-% Back emf at rated speed
-Ea = Kb*Nrated
+% Back emf at rated speed (line-to-line)
+Ea_ll = Kb*Nrated*1e-3; % V
+Ea = Ea_ll/sqrt(3); % V
+% Terminal voltage
+Vt = Vt_ll/sqrt(3);
 % Phase resistance
 Ra = Rlinetoline/2; % Ohms
+% Drive frequency
+
+%%
+% Datasheet parameters
+% KBM(S)-88X00-X - B
+rotor_pole = 46;
+Prated = 5460; % W
+Tstall_cont = 102; % Nm
+Tstall_peak = 197; % Nm
+Icont = 34; % Arms
+Nrated = 1000; % rpm
+Kt = 3.04; % Nm/Arms
+Kb = 184; % Vrms/krpm
+Rlinetoline = 0.165; %Ohms
+Lm = 1.6; % mH
+Vt_ll = 240; % V
+
+% Back emf at rated speed (line-to-line)
+Ea_ll = Kb*Nrated*1e-3; % V
+Ea = Ea_ll/sqrt(3); % V
+% Terminal voltage
+Vt = Vt_ll/sqrt(3);
+% Phase resistance
+Ra = Rlinetoline/2; % Ohms
+
+stator_slot = 36;
+phase = 3;
+
+% Drive frequency
 
