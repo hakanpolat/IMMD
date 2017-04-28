1cb138b4b1e9d629872763d866d5ec0fdd639eec
mesutto <mesutugur@gmail.com>
2017-04-28 19:18:55 +0300

    rpg loss generic code.

---
+++ Research League/Erencan/RPG 2017/simulation/calculate_loss.m
@@ -0,0 +1,18 @@
+function Ptotal = calculate_loss(Icpeak,Vdc)
+
+Vce_sat = 1+ 0.0093*Icpeak; % V
+
+
+% Eon = 6e-3; % J, @600V
+% Eon = Eon*Vdc/600; % J, @750V
+% Eoff = 10.4e-3; % J, @600V
+% Eoff = Eoff*Vdc/600; % J, @750V
+% Err = 8e-3; % J, @600V
+% Err = Err*Vdc/600; % J, @750V
+% Vec = 2.05; % V
+% trr = 150e-9; % s, @600V
+% trr = trr*Vdc/600; % s, @750V
+% 
+
+end
+
