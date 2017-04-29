2c697d635014c69f19ab8094ccdab8196825ae93
mesutto <mesutugur@gmail.com>
2017-04-29 19:28:57 +0300

    H matrix finished.

--- Research League/Erencan/Observability/matlab work/fnc3_createH.m
+++ Research League/Erencan/Observability/matlab work/fnc3_createH.m
@@ -4,7 +4,9 @@ branch_data = [Branch_number_from Branch_number_to];
 H = zeros(no_of_pi+no_of_pf,No_of_Buses);
 for k = 1:no_of_pi
     H(k,pi_vect(k)) = sum(sum((branch_data(:,1) == pi_vect(k))));
-    H(k,branch_data(find(branch_data(:,1)) == pi_vect(k))) = -1
+    H(k,branch_data(find(branch_data(:,1) == pi_vect(k)),2)) = -1;
+    H(k,pi_vect(k)) = H(k,pi_vect(k))+sum(sum((branch_data(:,2) == pi_vect(k))));
+    H(k,branch_data(find(branch_data(:,2) == pi_vect(k)),1)) = -1;
 end
 for k = no_of_pi+1:no_of_pi+no_of_pf
     H(k,pf_vect(k-no_of_pi,1)) = 1;
