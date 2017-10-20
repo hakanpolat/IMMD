12a5d2098b043ccbdc9e2b6359c49b77efa18842
Furkan Karakaya <furkan94karakaya@gmail.com>
2017-10-20 16:58:08 +0300

    GaN Modeleleme çalışmaları
    GaN modellemesinde umut vadeden sonuçlar elde edebildik :) Sonuçları .pdf dosyasında iletiyorum.

--- Project/3501/Furkan/GanModel_Nonsegmented.m
+++ Project/3501/Furkan/GanModel_Nonsegmented.m
@@ -16,17 +16,23 @@ c2 = 0.153;
 Cgd = 0.5e-12;
 Cgs = 64.5e-12;
 Cds = 16.5e-12;
+Ls = 10e-9;
+Ld = 10e-9;
 
 Rg = 1;
 Rd = 0.1;
 Rs = 0.1;
 %% Source parameters
-PulseAmplitude = 6;
+PulseAmplitude = 5;
 fsw = 100e3;
 Vdc = 300;
+D = 50; % duty cycle 
+
+%% Load parameters
+Rload = 10;
 %% Run Simulink
 SampleTime = 1e-12;
-model = 'GaNModel_Nonsegmented';
+model = 'GaN_SimScape_Nonsegmented';
 load_system(model);
-set_param(model, 'StopTime','4e-5' )
+set_param(model, 'StopTime','2e-5' )
 sim(model);
