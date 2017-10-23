1336ffdd55c28b34a21bc9bca513e600da8cf20e
Furkan Karakaya <furkan94karakaya@gmail.com>
2017-10-23 18:55:51 +0300

    Small Changes in Model

--- Project/3501/Furkan/GanModel_Nonsegmented.m
+++ Project/3501/Furkan/GanModel_Nonsegmented.m
@@ -16,15 +16,16 @@ c2 = 0.153;
 Cgd = 0.5e-12;
 Cgs = 64.5e-12;
 Cds = 16.5e-12;
-Ls = 10e-10;
-Ld = 10e-10;
+Ls = 1e-9;
+Ld = 1e-9;
+Lg = 1e-9;
 
 Rg = 1;
 Rd = 0.1;
 Rs = 0.1;
 %% Source parameters
-PulseAmplitude = 5;
-fsw = 100e3;
+PulseAmplitude = 10;
+fsw = 800e3;
 Vdc = 300;
 D = 50; % duty cycle 
 
@@ -34,5 +35,5 @@ Rload = 10;
 SampleTime = 1e-12;
 model = 'GaN_SimScape_Nonsegmented';
 load_system(model);
-set_param(model, 'StopTime','2e-5' )
+set_param(model, 'StopTime','2e-6' )
 sim(model);
