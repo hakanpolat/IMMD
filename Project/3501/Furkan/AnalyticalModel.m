dbb2740a3c2d945707d93ac6d61d3d4f1a55a9ce
Furkan Karakaya <furkan94karakaya@gmail.com>
2017-10-30 00:08:53 +0300

    Denemeler devam ediyor, iyi sonuçlara bir miktar yaklaşım var.

---
+++ Project/3501/Furkan/AnalyticalModel.m
@@ -0,0 +1,34 @@
+close all
+clear all
+clc
+Vgs = -3:1:6;
+Vds = -7:0.001:5;
+K1 = 0.283;
+b1 = 2.035;
+c1 = 0.124;
+d1 = 1.159;
+e1 = -0.204;
+m1 = 30.972;
+n1 = -4.48;
+K2 = 7.114;
+b2 = 2.054;
+c2 = 0.153;
+for GateIndex = 1:10
+    for i=1:((12/0.001)+1)
+        GS = Vgs(GateIndex);
+        DS = Vds(i);
+        GD = GS - DS;
+        if Vds(i)>0
+            I(GateIndex,i) = K1*log(1+exp((GS-b1)/c1))*(m1+n1*GS)*DS/(1+(d1+e1*GS)*DS);
+        else
+            I(GateIndex,i) = -K2*log(1+exp((GD-b2)/c2))*(-DS/(1-DS));
+        end
+    end
+end
+
+hold all
+for j=[1,2,6,9,10]
+    plot(Vds, I(j,:));
+end
+hold off
+legend ('Vgs = -3','Vgs = -2','Vgs = 2','Vgs = 5','Vgs = 6');
--- Project/3501/Furkan/HalfBridge.m
+++ Project/3501/Furkan/HalfBridge.m
@@ -17,9 +17,9 @@ Cgd = 2e-12;
 Cgs = 258e-12;
 Cds = 63e-12;
 
-Ls = 0.05e-9;
-Ld = 3e-9;
-Lg = 3e-9;
+Ls = 2e-9;
+Ld = 7e-9;
+Lg = 2e-9;
 
 Rg = 1;
 Rd = 25e-6;
@@ -29,21 +29,21 @@ Rs = 25e-6;
 Ron = 12;
 Roff = 2;
 %% Source parameters
-PulseAmplitude = 6;
+PulseAmplitude = 5;
 fsw = 1000e3;
 Vdc = 300;
 % Quantities in below are in percent
 Dtop = 49; % duty cycle of top
 Dbot = 49; % duty cycle of bot
-DelayTop = 0;
-DelayBot = 50;
+DelayTop = 50;
+DelayBot = 0;
 
 
 %% Load parameters
 Rload = 10;
-Lload = 10e-6;
+Lload = 1e-6;
 %% Run Simulink
-SampleTime = 1e-14;
+SampleTime = 0.5e-12;
 model = 'GaN_HalfBridge';
 load_system(model);
 set_param(model, 'StopTime','1.4e-6' )
