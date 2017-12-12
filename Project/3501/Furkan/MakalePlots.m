4586bc83cf7b9987ef200a91bb97c056013dcf0b
Furkan Karakaya <furkan94karakaya@gmail.com>
2017-12-12 14:01:45 +0300

    Makale Ozan hoca ve Mesut hocanın tavsiyeleri doğrultusunda iyileştirildi, haftalık toplantıda üzerinde konuşmak üzere hazırlandı.

--- Project/3501/Furkan/MakalePlots.m
+++ Project/3501/Furkan/MakalePlots.m
@@ -3,13 +3,13 @@
 %Indicate Xtick Ytick and check legend and labels
 fplot = figure;
 figure(fplot)
-plot((1:6000001)*SampleTime, 10*TopChInd,(1:6000001)*SampleTime, TopVoltDSCap,'Linewidth',3.0);
+plot((1:6000001)*SampleTime, -10*BotChInd,(1:6000001)*SampleTime, BotChVoltInd,'Linewidth',3.0);
 ylim([-60 500]);
 xlim([0.199E-5 0.204E-5]);
 ax = gca;
 ax.XTick = [0.199E-5 0.2E-5 0.201E-5 0.202E-5 0.203E-5 0.204E-5];
 ax.YTick = [0 100 200 300 400 500];
-legend('I_c_h (x10)','V_c_h');
+legend('-I_c_h (x10)','V_c_h');
 xlabel('Time(s)','FontSize',12);
 ylabel('Current(A), Voltage(V)');
 % grid on
