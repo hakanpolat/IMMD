0a130dfbeadc6b0f1f2bccfffd60e4e2e2199c2b
Furkan Karakaya <furkan94karakaya@gmail.com>
2018-02-25 17:31:07 +0300

    PWM section is completed successfully. Next task is energizing the GaN transistors.
    Weekly Report is added.
    
    Signed-off-by: Furkan Karakaya <furkan94karakaya@gmail.com>

---
+++ Project/3501/Furkan/DSP/Lab7/Debug/Lab7_10_CapturePWM.d
@@ -0,0 +1,43 @@
+# FIXED
+
+Lab7_10_CapturePWM.obj: ../Lab7_10_CapturePWM.c
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Device.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Adc.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DevEmu.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_CpuTimers.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECan.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECap.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DMA.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EPwm.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EQep.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Gpio.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_I2c.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_McBSP.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieCtrl.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieVect.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Spi.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Sci.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_SysCtrl.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_XIntrupt.h
+Lab7_10_CapturePWM.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Xintf.h
+
+../Lab7_10_CapturePWM.c: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Device.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Adc.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DevEmu.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_CpuTimers.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECan.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECap.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DMA.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EPwm.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EQep.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Gpio.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_I2c.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_McBSP.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieCtrl.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieVect.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Spi.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Sci.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_SysCtrl.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_XIntrupt.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Xintf.h: 
