b4b50e38527129b1c9a439d261b56bec946b8256
mesutto <mesutugur@gmail.com>
2018-03-07 23:29:37 +0300

    Some of the simulation results are added. the overall paper needs review

---
+++ Paper/PEMC 2018/Results/plot_pemc.m
@@ -0,0 +1,37 @@
+%%
+timea = 0:Sampling_time:stop_time;
+figure;
+plot(timea,voltec,'b -','Linewidth',1.5);
+% hold on
+% plot(ar_xaxis,ar_np_miron(2,:),'k -','Linewidth',3);
+% hold on;
+% plot(ar_xaxis,ar_np_miron(3,:),'r -','Linewidth',3);
+% hold on;
+% plot(ar_xaxis,ar_np_miron(4,:),'g -','Linewidth',3);
+% hold off;
+%grid on;
+set(gca,'FontSize',14);
+%legend('Qs=12','Qs=18','Qs=24','Qs=30','Qs=36','Qs=42','Qs=48');
+%legend('1 parallel','2 parallel','3 parallel','4 parallel');
+%title('w = 2, ns = 2');
+xlabel('Time (s)','FontSize',14,'FontWeight','Bold')
+ylabel('Voltage (Volts)','FontSize',14,'FontWeight','Bold')
+xlim([0.297 0.317]);
+
+figure;
+plot(timea,akim,'b -','Linewidth',1.5);
+% hold on
+% plot(ar_xaxis,ar_np_miron(2,:),'k -','Linewidth',3);
+% hold on;
+% plot(ar_xaxis,ar_np_miron(3,:),'r -','Linewidth',3);
+% hold on;
+% plot(ar_xaxis,ar_np_miron(4,:),'g -','Linewidth',3);
+% hold off;
+%grid on;
+set(gca,'FontSize',14);
+%legend('Qs=12','Qs=18','Qs=24','Qs=30','Qs=36','Qs=42','Qs=48');
+%legend('1 parallel','2 parallel','3 parallel','4 parallel');
+%title('w = 2, ns = 2');
+xlabel('Time (s)','FontSize',14,'FontWeight','Bold')
+ylabel('Current (Amps)','FontSize',14,'FontWeight','Bold')
+xlim([0.3 0.32]);
