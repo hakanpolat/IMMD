82a635401453dec0ce0d2f6f904d2b06fd934a6b
DESKTOP-0O5V01O\Furkan <furkan.karakaya@metu.edu.tr>
2018-06-04 16:19:58 +0300

    EPE - Experimental results are plotted for meeting.

--- Project/3501/Furkan/GaN Model/Experimental Results/Plots.m
+++ Project/3501/Furkan/GaN Model/Experimental Results/Plots.m
@@ -3,7 +3,7 @@ f1 = figure('Name','Experimental Results','units','normalized','outerposition',[
 %% Bot Turn OFF
 %f1 = figure('Name','Bottom Switch Turn OFF Graph');
 %figure(f1);
-    subplot(2,2,1)
+    ha(1) = subplot(2,2,1)
     hold all
     grid on
     ax = gca;
@@ -13,13 +13,13 @@ f1 = figure('Name','Experimental Results','units','normalized','outerposition',[
     ax.FontSize = 12;
 %     ax.YTick = [0:20:120];
 %     xlim([0 5.1])
-    SampleRange = 2499;
-    SampleMid = 2500;
+    SampleRange = 12499;
+    SampleMid = 12500;
     SampleBeg = SampleMid - SampleRange;
     SampleEnd = SampleMid + SampleRange;
-    plot( (SampleBeg:SampleEnd)/5e9 , 0.1*BotOFFVdsBot(SampleBeg:SampleEnd,2),'LineWidth',3.0);
+    plot( (SampleBeg:SampleEnd)/5e9 , 0.1*TopONVdsBot(SampleBeg:SampleEnd,2),'LineWidth',3.0);
     plot( (SampleBeg:SampleEnd)/5e9 , BotOFFVgsBot(SampleBeg:SampleEnd,2),'LineWidth',3.0);    
-    plot( (SampleBeg:SampleEnd)/5e9 , 7.7/0.351*BotOFFV25m(SampleBeg:SampleEnd,2),'LineWidth',3.0);    
+    plot( (SampleBeg:SampleEnd)/5e9 , 7.811/0.3625*BotOFFV25m(SampleBeg:SampleEnd,2),'LineWidth',3.0);    
     plot( (SampleBeg:SampleEnd)/5e9 , 10*BotOFFIload(SampleBeg:SampleEnd,2),'LineWidth',3.0);  
     legend('Vds(V)','Vgs(V)','Ids(A)','ILoad(A)','Location','Best');
     hold off
@@ -27,7 +27,7 @@ f1 = figure('Name','Experimental Results','units','normalized','outerposition',[
 %% Bot Turn ON    
 % f2 = figure('Name','Bottom Switch Turn ON Graph');
 % figure(f2);
-    subplot(2,2,2)
+    hb(1) = subplot(2,2,2)
     hold all
     grid on
     ax = gca;
@@ -37,13 +37,13 @@ f1 = figure('Name','Experimental Results','units','normalized','outerposition',[
     ax.FontSize = 12;
 %     ax.YTick = [0:20:120];
 %     xlim([0 5.1])
-    SampleRange = 4999;
-    SampleMid = 5000;
+    SampleRange = 12499;
+    SampleMid = 12500;
     SampleBeg = SampleMid - SampleRange;
     SampleEnd = SampleMid + SampleRange;
-    plot( (SampleBeg:SampleEnd)/5e9 , 0.1*BotONVdsBot(SampleBeg:SampleEnd,2),'LineWidth',3.0);
+    plot( (SampleBeg:SampleEnd)/5e9 , 0.1*TopOFFVdsBot(SampleBeg:SampleEnd,2),'LineWidth',3.0);
     plot( (SampleBeg:SampleEnd)/5e9 , BotONVgsBot(SampleBeg:SampleEnd,2),'LineWidth',3.0);    
-    plot( (SampleBeg:SampleEnd)/5e9 , 7.7/0.351*BotONV25m(SampleBeg:SampleEnd,2),'LineWidth',3.0);
+    plot( (SampleBeg:SampleEnd)/5e9 , 7.811/0.3625*BotONV25m(SampleBeg:SampleEnd,2),'LineWidth',3.0);
     plot( (SampleBeg:SampleEnd)/5e9 , 10*BotONIload(SampleBeg:SampleEnd,2),'LineWidth',3.0);  
     legend('Vds(V)','Vgs(V)','Ids(A)','ILoad(A)','Location','Best');
     hold off
@@ -51,7 +51,7 @@ f1 = figure('Name','Experimental Results','units','normalized','outerposition',[
 %% Top Turn OFF
 % f3 = figure('Name','Top Switch Turn OFF Graph');
 % figure(f3);
-    subplot(2,2,4)  
+    hb(2) = subplot(2,2,4)  
     hold all
     grid on
     ax = gca;
@@ -68,14 +68,14 @@ f1 = figure('Name','Experimental Results','units','normalized','outerposition',[
     plot( (SampleBeg:SampleEnd)/5e9 , 0.1*TopOFFVdsTop(SampleBeg:SampleEnd,2),'LineWidth',3.0);
     plot( (SampleBeg:SampleEnd)/5e9 , TopOFFVgsTop(SampleBeg:SampleEnd,2),'LineWidth',3.0);    
     plot( (SampleBeg:SampleEnd)/5e9 , TopOFFIdsTop(SampleBeg:SampleEnd,2),'LineWidth',3.0);
-    plot( (SampleBeg:SampleEnd)/5e9 , 10*TopOFFIload(SampleBeg:SampleEnd,2),'LineWidth',3.0);  
+    plot( (SampleBeg:SampleEnd)/5e9 , 10*BotONIload(SampleBeg:SampleEnd,2),'LineWidth',3.0);  
     legend('Vds(V)','Vgs(V)','Ids(A)','ILoad(A)','Location','Best');
     hold off
     %saveas(f1, 'I_{DS} vs V_{DS} Graph for Different Temperatures @ V_{gs} = 6V.jpg');
 %% Top Turn ON
 % f4 = figure('Name','Top Switch Turn ON Graph');
 % figure(f4);
-    subplot(2,2,3)
+    ha(2) = subplot(2,2,3)
     hold all
     grid on
     ax = gca;
@@ -92,10 +92,12 @@ f1 = figure('Name','Experimental Results','units','normalized','outerposition',[
     plot( (SampleBeg:SampleEnd)/5e9 , 0.1*TopONVdsTop(SampleBeg:SampleEnd,2),'LineWidth',3.0);
     plot( (SampleBeg:SampleEnd)/5e9 , TopONVgsTop(SampleBeg:SampleEnd,2),'LineWidth',3.0);    
     plot( (SampleBeg:SampleEnd)/5e9 , TopONIdsTop(SampleBeg:SampleEnd,2),'LineWidth',3.0);
-    plot( (SampleBeg:SampleEnd)/5e9 , 10*TopONIload(SampleBeg:SampleEnd,2),'LineWidth',3.0);  
+    plot( (SampleBeg:SampleEnd)/5e9 , 10*BotOFFIload(SampleBeg:SampleEnd,2),'LineWidth',3.0);  
     legend('Vds(V)','Vgs(V)','Ids(A)','ILoad(A)','Location','Best');
     hold off
     %saveas(f1, 'I_{DS} vs V_{DS} Graph for Different Temperatures @ V_{gs} = 6V.jpg'); 
 %% Dock Figures
+    linkaxes(ha, 'x');
+    linkaxes(hb, 'x');
 % h = [f1,f2,f3,f4];
 % set(h,'WindowStyle','Docked');
