8a94f07bc90dc856189c218bba99784a7135917a
DESKTOP-0O5V01O\Furkan <furkan.karakaya@metu.edu.tr>
2018-08-07 16:27:34 +0300

    State space model is going well but there are something to be solved.

--- Project/3501/Furkan/GaN Model/StateSpaceGaNChargeBased.m
+++ Project/3501/Furkan/GaN Model/StateSpaceGaNChargeBased.m
@@ -9,8 +9,6 @@ Ls = 0.9e-9;
 Rs = 3.6 * 0.238 * 0.82 * (1 - (-0.0135*(25 - 25))) / 295;
 Ld = 0.9e-9;
 Rd = (3.6/8) * (0.95*0.82*(1 - (-0.0135*(25 - 25))) * 18.2 / 295);
-Cgs = 100e-9;
-Cds = 100e-9;
 
 %% Simulation Parameters
 TimeStep = 1e-12; %Time Steps
@@ -34,33 +32,34 @@ x14 = zeros(size(t));%Qds
 u1 = zeros(size(t)); %Vgss
 
 %% Input Definition
-u1(t>=50e-9) = 6; 
-x2(t>=0) = 1;
+u1(t>=10e-9) = 6; 
+x2(t>=20e-9) = 0;
 x3 = x2;
-x10(1) = Cgs;
-x11(1) = Cds;
+[x8(1),x9(1),x10(1),x11(1)] = NumericCalc(x5(1),x3(1));
+x12(1) = x9(1) * (x5(1) - x3(1));
+x13(1) = x10(1) * x5(1);
+x14(1) = x11(1) * x3(1);
 [m,n] = size(t);
 for k=2:n-1
     
-    if k ~= 0
-        [x8(k),x9(k),x10(k),x11(k)] = NumericCalc(x13(k)/x10(k-1),x14(k)/x11(k-1));
-    else 
-        [x8(k),x9(k),x10(k),x11(k)] = NumericCalc(x13(k)/x10(k-1),x14(k)/x11(k-1));
-    end
+    VgsTemp = x13(k-1)/x10(k-1);
+    VdsTemp = x14(k-1)/x11(k-1);
+    [x8(k),x9(k),x10(k),x11(k)] = NumericCalc(VgsTemp,VdsTemp);
 
+    Dx1 = TimeStep * ((x2(k-1) - x1(k-1)*(Rs+Rd) - x14(k-1)/x11(k))/(Ls + Ld));
+    Dx7 = TimeStep * ((u1(k) - x13(k-1)/x10(k))/(Lgin + Lss) - (Rgin + Rss)/(Lgin + Lss)*x7(k-1));
+    Dx12 = TimeStep * ((x7(k-1)/x10(k) - x1(k-1)/x11(k) + x8(k)/x11(k)) / (1/x10(k) + 1/x9(k) + 1/x11(k)));
+    Dx13 = TimeStep * (x7(k-1) + (x1(k-1)/x11(k) - x7(k-1)/x10(k) - x8(k)/x11(k) ) / (1/x10(k) + 1/x9(k) + 1/x11(k)));
+    Dx14 = TimeStep * (x1(k-1) - x8(k) + (x7(k-1)/x10(k) - x1(k-1)/x11(k))/(1/x10(k) + 1/x9(k) + 1/x11(k)));
+
+    x1(k) = x1(k-1) + Dx1;
+    x7(k) = x7(k-1) + Dx7;
+    x12(k) = x12(k-1) + Dx12;
+    x13(k) = x13(k-1) + Dx13;
+    x14(k) = x14(k-1) + Dx14;
+    
 end
 
-Dx1 = TimeStep * ((x2(k-1) - x1(k-1)*(Rs+Rd) - x14(k-1)/x11(k))/(Ls + Ld));
-Dx7 = TimeStep * ((u1(k) - x13(k-1)/x10(k))/(Lgin + Lss) - (Rgin + Rss)/(Lgin + Lss)*x7(k-1));
-Dx12 = TimeStep * ((x7(k-1)/x10(k) - x1(k-1)/x11(k) + x8(k)/x11(k)) / (1/x10(k) + 1/x9(k) + 1/x11(k)));
-Dx13 = TimeStep * (x7(k-1) + (x1(k-1)/x11(k) - x7(k-1)/x10(k) - x8(k)/x11(k) ) / (1/x10(k) + 1/x9(k) + 1/x11(k)));
-Dx14 = TimeStep * (x1(k-1) - x8(k) + (x7(k-1)/x10(k) - x1(k-1)/x11(k))/(1/x10(k) + 1/x9(k) + 1/x11(k)));
-
-x1(k) = x1(k-1) + Dx1;
-x7(k) = x7(k-1) + Dx7;
-x12(k) = x12(k-1) + Dx12;
-x13(k) = x13(k-1) + Dx13;
-x14(k) = x14(k-1) + Dx14;
 
 
 
@@ -84,10 +83,43 @@ plot(t,x8,t,x14./x11,t,x13./x10,'Linewidth',2.0);
 ylim([-110 110]);
 xlabel('Time');
 ylabel('Voltage,Ampere');
-title({'Ids, Vds, Vgs IN'})
-legend ('Ids','Vds','Vgs','Location','best');
+title({'Ich, Vds, Vgs IN'})
+legend ('Ich','Vds','Vgs','Location','best');
 hold off
 
+figure;
+hold all
+grid on
+plot(t,x7,t,x8,t,x13./x10,'Linewidth',2.0);
+%xlim([0]);
+ylim([-110 110]);
+xlabel('Time');
+ylabel('Voltage,Ampere');
+title({'Ich, Vds, Vgs IN'})
+legend ('Ig','Ich','Vgs','Location','best');
+hold off
 
+figure;
+hold all
+grid on
+plot(t,x9,t,x10,t,x11,'Linewidth',2.0);
+%xlim([0]);
+ylim([-2e-9 2e-9]);
+xlabel('Time');
+ylabel('Voltage,Ampere');
+title({'Cgd, Cgs, Cds IN'})
+legend ('Cgd','Cgs','Cds','Location','best');
+hold off
 
+figure;
+hold all
+grid on
+plot(t,x12,t,x13,t,x14,'Linewidth',2.0);
+%xlim([0]);
+ylim([-2e-9 20e-9]);
+xlabel('Time');
+ylabel('Voltage,Ampere');
+title({'Qgd, Qgs, Qds IN'})
+legend ('Qgd','Qgs','Qds','Location','best');
+hold off
 
