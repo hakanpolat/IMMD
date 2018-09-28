4b9be35234ffa85c794a6599fbf381e0d0f3aa0c
DESKTOP-0O5V01O\Furkan <furkan.karakaya@metu.edu.tr>
2018-09-28 16:40:51 +0300

    Simulation speed is now better. Discussion about variable sample time implemented in the state-space code is required.

--- Project/3501/Furkan/GaN Model/StateSpaceReducedElements.m
+++ Project/3501/Furkan/GaN Model/StateSpaceReducedElements.m
@@ -1,12 +1,13 @@
 % State Variables
 % Id, Ig, Vgsin, Vdsin
 %% State Space Model of GaN
-clear all;
+% clear all;
 % close all;
 
 profile on
 tic
 %% GaN Parameters
+
 Rgin = 1.5;
 Lgin = 0.65e-9;
 Rss = 1e-3;
@@ -33,47 +34,61 @@ x7 = zeros(size(t)); %Ig
 u1 = zeros(size(t)); %Vgss
 u2 = zeros(size(t)); %Vdc
 
+%% Creating Matrices
+A = zeros(size(4));
+    A(1,1) = -(Rs+Rd)/(Ls+Ld);
+    A(1,4) = -1/(Ls+Ld);
+    A(2,2) = -(Rgin+Rss)/(Lss+Lgin);
+    A(2,3) = -1/(Lgin + Lss);
+B = zeros(size(4,3));
+    B(1,2) = 1/(Ls+Ld);
+    B(2,1) = 1/(Lgin+Lss);
+CurrVect = zeros(size(4,1));
+NextVect = zeros(size(3,1));
 %% Input Definition
-% u1(t>=50e-9) = 6;
+u1(t>=50e-9) = 6;
 u2(t>=0e-9) = 2;
-% u1(t>=200e-9) = -3;
+u1(t>=200e-9) = -3;
 % u1(t>=400e-9) = 6;
 % u2(t>=200e-9) = 0;
 x3 = u2(1);
 x4 = u1(1);
 [m,n] = size(t);
 for k = 2:n-1
-%     [x8, x9, x10, x11] = NumericCalc(x4,x3);
-   A(1:4) = NumericCalc(x4,x3);
-   x8 = A(1);
-   x9 = A(2);
-   x10 = A(3);
-   x11 = A(4);
-%     x9(k) = 2e-12;
-%     x10(k) = 258e-12;
-%     x11(k) = 63e-12;
-    CC = x9*x10 + x9*x11 + x10*x11; %
-    a11 = -(Rs+Rd)/(Ls+Ld);
-    a13 = 0;
-    a14 = -1/(Ls+Ld);
-    a22 = -(Rgin+Rss)/(Lss+Lgin);
-    a23 = -1/(Lgin + Lss);
-    a31 = x9/CC;
-    a32 = (x11+x9)/CC;
-    a41 = (x9 + x10)/CC;
-    a42 = -1/x9 + (x11 + x9)*(x10+x9)/(x9*CC);
+    [x8, x9, x10, x11] = NumericCalc(x4,x3);
+%    A(1:4) = NumericCalc(x4,x3);
+%    x8 = A(1);
+%    x9 = A(2);
+%    x10 = A(3);
+%    x11 = A(4);
+%    x9(k) = 2e-12;
+%    x10(k) = 258e-12;
+%    x11(k) = 63e-12;
+
+    CC = x9*x10 + x9*x11 + x10*x11; 
+
+    A(3,1) = x9/CC;
+    A(3,2) = (x11+x9)/CC;
+    A(4,1) = (x9 + x10)/CC;
+    A(4,2) = -1/x9 + (x11 + x9)*(x10+x9)/(x9*CC);
     
-    A = [a11 0 a13 a14;0 a22 a23 0;a31 a32 0 0;a41 a42 0 0];
+%     A = [a11 0 a13 a14;0 a22 a23 0;a31 a32 0 0;a41 a42 0 0];
     
-    b12 = 1/(Ls+Ld);
-    b21 = 1/(Lgin+Lss);
-    b33 = -x9/CC;
-    b43 = -(x9 + x10)/CC;
+
+    B(3,3) = -x9/CC;
+    B(4,3) = -(x9 + x10)/CC;
     
-    B = [0 b12 0;b21 0 0;0 0 b33;0 0 b43];
+%     B = [0 b12 0;b21 0 0;0 0 b33;0 0 b43];
  
-    CurrVect = [x1(k-1);x7(k-1);x4;x3];
-    InpVect = [u1(k); u2(k); x8];
+    CurrVect(1,1) = x1(k-1);
+    CurrVect(2,1) = x7(k-1);
+    CurrVect(3,1) = x4;
+    CurrVect(4,1) = x3;
+    
+    InpVect(1,1) = u1(k);
+    InpVect(2,1) = u2(k);
+    InpVect(3,1) = x8;
+
 
     % Backward Euler Solution
 %     NextVect = inv(eye(4) - A*SampleTime)*(CurrVect + B*InpVect*SampleTime);
@@ -89,8 +104,7 @@ for k = 2:n-1
     x7(k) = NextVect(2,1);
     x4 = NextVect(3,1);
     x3 = NextVect(4,1);
-    
-    
+
 end
 toc
 profile off
