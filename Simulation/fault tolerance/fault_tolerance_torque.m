0c3f5a7af697e3ba849bac147295e7a383405753
mesutto <mesutugur@gmail.com>
2018-10-21 15:42:21 +0300

    Fault'lar üzerine epey bir çalışma yapıldı.

--- Simulation/fault tolerance/fault_tolerance_torque.m
+++ Simulation/fault tolerance/fault_tolerance_torque.m
@@ -6,9 +6,9 @@ p = 10; % pole number
 Nm = 120*fo/p; % rpm
 wm = Nm*2*pi/60; % rad/sec
 phase = 0*pi/180; % rad
-shift = 0*pi/180; % rad
-%scale = sqrt(3);
-scale = 1;
+shift = 30*(pi/180); % rad
+scale = sqrt(3);
+%scale = 1;
 
 % time-axis
 t = 0:1e-6:0.1; % s
@@ -21,8 +21,8 @@ Ec = Ep*sin(2*pi*fo*t+2*pi/3); % V
 % phase currents
 Ia = scale*Ip*sin(2*pi*fo*t-phase-shift); % V
 Ib = scale*Ip*sin(2*pi*fo*t-2*pi/3-phase+shift); % V
-Ic = Ip*sin(2*pi*fo*t+2*pi/3-phase); % V
-%Ic = 0;
+%Ic = Ip*sin(2*pi*fo*t+2*pi/3-phase); % V
+Ic = zeros(1,numel(Ia));
 
 Tma = Ea.*Ia/wm;
 Tmb = Eb.*Ib/wm;
@@ -50,3 +50,86 @@ ylim([-5 30])
 
 %Torque = 3/2*Ip*Ep/wm*cos(phase)
 
+
+figure;
+hold all;
+%plot(t,Ia*0.5,'b--','Linewidth',2)
+plot(t,Ia,'b-','Linewidth',2);
+plot(t,Ib,'r-','Linewidth',2);
+plot(t,Ic,'g-','Linewidth',2);
+grid on;
+set(gca,'FontSize',12);
+xlabel('Time (s)','FontSize',12,'FontWeight','Bold')
+ylabel('Current (A)','FontSize',12,'FontWeight','Bold')
+legend('Phase-A','Phase-B','Phase-C');
+xlim([0.06 0.1])
+ylim([-20 20])
+
+
+%%
+% 3-phase drive (PMSM)
+Ep = 100; % V
+Ip = 10; % A
+fo = 50; % Hz
+p = 10; % pole number
+Nm = 120*fo/p; % rpm
+wm = Nm*2*pi/60; % rad/sec
+phase = 0*pi/180; % rad
+%shift = 30*(pi/180); % rad
+shift = 0*(pi/180); % rad
+%scale = sqrt(3);
+scale = 1;
+
+% time-axis
+t = 0:1e-6:0.1; % s
+
+% induced voltages
+Ea = Ep*sin(2*pi*fo*t); % V
+Eb = Ep*sin(2*pi*fo*t-2*pi/3); % V
+Ec = Ep*sin(2*pi*fo*t+2*pi/3); % V
+
+% phase currents
+Ia = scale*Ip*sin(2*pi*fo*t-phase-shift); % V
+Ib = scale*Ip*sin(2*pi*fo*t-2*pi/3-phase+shift); % V
+%Ic = Ip*sin(2*pi*fo*t+2*pi/3-phase); % V
+Ic = zeros(1,numel(Ia));
+
+Tma = Ea.*Ia/wm;
+Tmb = Eb.*Ib/wm;
+Tmc = Ec.*Ic/wm;
+Tm = Tma + Tmb + Tmc;
+
+theta = 2*pi*fo*t;
+Torque = (Ip*Ep/wm)*( 1 - cos(2*theta)/4 + sqrt(3)/4*sin(2*theta));
+
+figure;
+hold all;
+%plot(t,Ia*0.5,'b--','Linewidth',2)
+plot(t,Tma,'b-','Linewidth',2);
+plot(t,Tmb,'r-','Linewidth',2);
+plot(t,Tmc,'g-','Linewidth',2);
+plot(t,Tm,'k-','Linewidth',3);
+%plot(t,Torque,'r-','Linewidth',3);
+grid on;
+set(gca,'FontSize',12);
+xlabel('Time (s)','FontSize',12,'FontWeight','Bold')
+ylabel('Torque (Nm)','FontSize',12,'FontWeight','Bold')
+legend('Phase-A','Phase-B','Phase-C','Total');
+xlim([0.06 0.1])
+ylim([-5 30])
+
+%Torque = 3/2*Ip*Ep/wm*cos(phase)
+
+figure;
+hold all;
+%plot(t,Ia*0.5,'b--','Linewidth',2)
+plot(t,Ia,'b-','Linewidth',2);
+plot(t,Ib,'r-','Linewidth',2);
+plot(t,Ic,'g-','Linewidth',2);
+grid on;
+set(gca,'FontSize',12);
+xlabel('Time (s)','FontSize',12,'FontWeight','Bold')
+ylabel('Current (A)','FontSize',12,'FontWeight','Bold')
+legend('Phase-A','Phase-B','Phase-C');
+xlim([0.06 0.1])
+ylim([-15 15])
--- Simulation/fault tolerance/inverter_fault_sim.m
+++ Simulation/fault tolerance/inverter_fault_sim.m
@@ -12,6 +12,7 @@ Ls = 3e-3; % Henries
 %Rs = 0.277; % Ohms
 Rs = 0.277e-6; % Ohms
 Nr = 600; %rpm
+wm = Nr*pi/30;
 p = 10;
 Tm = 127; % Nm
 fout = Nr*p/60;
@@ -40,10 +41,110 @@ Jm = 0.003; % kgm^2
 Fs = 0.0005; % Nms
 Foct = 0;
 Fsct = 0;
-Focw = 0;
+Focw = 1;
 Fscw = 0;
+Roc = 1e4;
 
 %%
 sim('inverter_fault_sim.slx');
+
+
+%%
+% Short circuit limiting trial
+Ts = 1e-6; % s
+Tfinal = 0.2; % s
+Tfault = 0; % s
+Ripth = 0.15; % s
+fsw = 10e3; % Hz
+Vdc = 540; % Volts
+Pout = 7e3/0.96; % W
+Ls = 3e-3; % Henries
+%Rs = 0.277; % Ohms
+Rs = 0.277e-6; % Ohms
+Nr = 600; %rpm
+wm = Nr*pi/30;
+p = 10;
+Tm = 127; % Nm
+fout = Nr*p/60;
+wout = 2*pi*fout; % rad/sec
+m = 3;
+np = 1;
+ns = 1;
+n = ns*np;
+Poutm = Pout/n; % Watts
+Xs = wout*Ls; % Ohms
+Vdcm = Vdc/ns; % volts
+
+scale = 1;
+scale2 = 1;
+%shift = 30*pi/180;
+shift = 0;
+
+Efnom = 160; % Volts
+Isnom = Poutm/(Ef*m); % Amps
+Iam = Isnom*scale;
+Ibm = Isnom*scale;
+Icm = Isnom*scale2;
+Iap = 0-shift;
+Ibp = -2*pi/3+shift;
+Icp = 2*pi/3;
+Eam = Efnom;
+Ebm = Efnom;
+Ecm = Efnom;
+Eap = 0;
+Ebp = -120*pi/180;
+Ecp = -240*pi/180;
+Ia = Iam*cos(Iap) + 1i*Iam*sin(Iap);
+Ib = Ibm*cos(Ibp) + 1i*Ibm*sin(Ibp);
+Ic = Icm*cos(Icp) + 1i*Icm*sin(Icp);
+Efa = Eam*cos(Eap) + 1i*Eam*sin(Eap);
+Efb = Ebm*cos(Ebp) + 1i*Ebm*sin(Ebp);
+Efc = Ecm*cos(Ecp) + 1i*Ecm*sin(Ecp);
+Vdropa = Ia*1i*Xs;
+Vdropb = Ib*1i*Xs;
+Vdropc = Ic*1i*Xs;
+Vta = Efa + Vdropa;
+Vtam = abs(Vta);
+Vtap = angle(Vta);
+deltada = Vtap*180/pi;
+Vtb = Efb + Vdropb;
+Vtbm = abs(Vtb);
+Vtbp = angle(Vtb);
+deltadb = Vtbp*180/pi;
+Vtc = Efc + Vdropc;
+Vtcm = abs(Vtc);
+Vtcp = angle(Vtc);
+deltadc = Vtcp*180/pi;
+maa = Vtam*sqrt(3)/(Vdcm*0.612);
+mab = Vtbm*sqrt(3)/(Vdcm*0.612);
+mac = Vtcm*sqrt(3)/(Vdcm*0.612);
+
+pf = cos(delta);
+phase = [0 90 0 90];
+Rin = 10;
+Vin = Vdc + Rin*(Pout/Vdc);
+Cdc = 200e-6;
+So = Pout/pf; % VA
+Vcon = Ef*sqrt(2)*sqrt(3)/Nr*1000;
+Jm = 0.003; % kgm^2
+Fs = 0.0005; % Nms
+
+Foct = 0;
+Fsct = 0;
+Focw = 1;
+Fscw = 0;
+Roc = 1e4;
+
+%%
+Vtab = Vta-Vtb;
+Vtbc = Vtb-Vtc;
+Vtca = Vtc-Vta;
+Vtabm = abs(Vtab)
+Vtbcm = abs(Vtbc)
+Vtcam = abs(Vtca)
+Vtabp = angle(Vtab)*180/pi
+Vtbcp = angle(Vtbc)*180/pi
+Vtcap = angle(Vtca)*180/pi
+
 
 
