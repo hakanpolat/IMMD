e3a99e496fcd6f551c0ff6dcc691a72d3dca475c
mesutto <mesutugur@gmail.com>
2019-05-06 22:59:11 +0300

    Dergi makalesi için experimental result source dosyaları. fig dosyaları büyük boyutlu olduğu için driveda bi yere koydum. @ozank, @furkankarakaya.

---
+++ Paper/IET JoPE 2019/calculations/capacitor.m
@@ -0,0 +1,26 @@
+Is = 8.8; % A
+pf = 0.97;
+ma = 0.9;
+fsw = 40e3; % Hz
+Vdcrip = 1; % percent
+ns = 2;
+np = 2;
+m = 3;
+
+Idcrms = Is*sqrt( 2*ma*(sqrt(3)/(4*pi) +...
+    pf^2*(sqrt(3)/pi-9*ma/16)) ) % Amps
+
+Cdcreq = ns*(ma*Is/(16*fsw*Vdcrip))*...
+    sqrt( (6 - (96*sqrt(3)*ma)/(5*pi) +...
+    (9*ma^2/2) )*pf^2 + (8*sqrt(3)*ma)/(5*pi) )/np/m % Farads
+
+Idcpercap = Idcrms/2;
+ESR = 21.1e-3; % Ohms
+Ta = 35;
+Rthcap = 35.72; % C/W
+Pcap = Idcpercap^2*ESR % W
+Pcap_tot = Pcap*3 % W
+Tcap = Ta + Pcap*Rthcap
+
+% Caprating: 5A,5uF
+
--- Prototype/Test/test diaries/2019.04.15/cizdir.m
+++ Prototype/Test/test diaries/2019.04.15/cizdir.m
@@ -7,27 +7,28 @@ PhC_Vds = [28.1,57.3,92.9,127.3,154.2,173.6,194.0,212.5,234.0,255.2,279.0,304.0]
 figure;
 hold all;
 plot(Vdc,PhA_Vds,'ro-','Linewidth',2);
-plot(Vdc,PhB_Vds,'b*-','Linewidth',2);
-plot(Vdc,PhC_Vds,'k--','Linewidth',2);
+plot(Vdc,PhB_Vds,'bx-','Linewidth',2);
+plot(Vdc,PhC_Vds,'k+-','Linewidth',2);
 grid on;
-set(gca,'FontSize',12);
-xlabel('DC Link Voltage (Volts)','FontSize',12,'FontWeight','Bold')
-ylabel('Vds (Volts)','FontSize',12,'FontWeight','Bold')
-xlim([0 325]);
-legend('Phase-A','Phase-B','Phase-C');
+set(gca,'FontSize',14);
+xlabel('DC Link Voltage (Volts)','FontSize',14,'FontWeight','Bold')
+ylabel('Maximum Drain-Source Voltage (Volts)','FontSize',14,'FontWeight','Bold')
+xlim([0 310]);
+legend({'Phase-A','Phase-B','Phase-C'},'Location','best');
 
+%%
 figure;
 hold all;
 plot(Vdc,PhA_Vds-Vdc,'ro-','Linewidth',2);
-plot(Vdc,PhB_Vds-Vdc,'b*-','Linewidth',2);
-plot(Vdc,PhC_Vds-Vdc,'k--','Linewidth',2);
+plot(Vdc,PhB_Vds-Vdc,'bx-','Linewidth',2);
+plot(Vdc,PhC_Vds-Vdc,'k+-','Linewidth',2);
 grid on;
-set(gca,'FontSize',12);
-xlabel('DC Link Voltage (Volts)','FontSize',12,'FontWeight','Bold')
-ylabel('Overshoot (Volts)','FontSize',12,'FontWeight','Bold')
-xlim([0 325]);
+set(gca,'FontSize',14);
+xlabel('DC Link Voltage (Volts)','FontSize',14,'FontWeight','Bold')
+ylabel('Peak Overshoot (Volts)','FontSize',14,'FontWeight','Bold')
+xlim([0 310]);
 ylim([0 50]);
-legend('Phase-A','Phase-B','Phase-C');
+legend({'Phase-A','Phase-B','Phase-C'},'Location','best');
 
 
 %% Vgs false-turn-ons
@@ -39,15 +40,15 @@ PhC_Vgs = [-0.99,0.113,0.527,0.549,0.666,0.755,0.858,0.919,0.949,0.993,1.13,1.12
 figure;
 hold all;
 plot(Vdc,PhA_Vgs,'ro-','Linewidth',2);
-plot(Vdc,PhB_Vgs,'b*-','Linewidth',2);
-plot(Vdc,PhC_Vgs,'k--','Linewidth',2);
-plot(Vdc,1.7*ones(1,numel(Vdc)),'m-','Linewidth',1);
-grid on;
-set(gca,'FontSize',12);
-xlabel('DC Link Voltage (Volts)','FontSize',12,'FontWeight','Bold')
-ylabel('Vgs (Volts)','FontSize',12,'FontWeight','Bold')
-xlim([0 325]);
-legend('Phase-A','Phase-B','Phase-C','Threshold');
+plot(Vdc,PhB_Vgs,'bx-','Linewidth',2);
+plot(Vdc,PhC_Vgs,'k+-','Linewidth',2);
+plot(Vdc,1.7*ones(1,numel(Vdc)),'g--','Linewidth',2);
+%grid on;
+set(gca,'FontSize',14);
+xlabel('DC Link Voltage (Volts)','FontSize',14,'FontWeight','Bold')
+ylabel('Peak Gate-Source Voltage (Volts)','FontSize',14,'FontWeight','Bold')
+xlim([0 310]);
+legend({'Phase-A','Phase-B','Phase-C','Threshold'},'Location','best');
 
 
 %%
@@ -55,58 +56,77 @@ legend('Phase-A','Phase-B','Phase-C','Threshold');
 
 figure;
 hold all;
-plot(e008*1e9,VarName2,'b-','Linewidth',2);
-plot(e1*1e9,VarName1,'r-','Linewidth',2);
-plot(e2*1e9,VarName3,'g-','Linewidth',1);
-grid on;
-set(gca,'FontSize',12);
-xlabel('Time (ns)','FontSize',12,'FontWeight','Bold')
-ylabel('Vds (Volts)','FontSize',12,'FontWeight','Bold')
+plot(e008*1e9,VarName2,'r-','Linewidth',2);
+plot(e1*1e9,VarName1,'b-','Linewidth',2);
+plot(e2*1e9,VarName3,'k-','Linewidth',2);
+plot(e2*1e9,300*ones(1,numel(e1)),'g--','Linewidth',2);
+%grid on;
+set(gca,'FontSize',14);
+xlabel('Time (ns)','FontSize',14,'FontWeight','Bold')
+ylabel('Drain-Source Voltage (Volts)','FontSize',14,'FontWeight','Bold')
 xlim([-5 60]);
-legend('Phase-A','Phase-B','Phase-C');
+legend({'Phase-A','Phase-B','Phase-C','DC Link Voltage'},'Location','best');
 
 %%
 % Vgs at 300Vdc
 
 figure;
 hold all;
-plot(e4*1e9,VarName6,'b-','Linewidth',2);
-plot(e5*1e9-2,VarName7,'r-','Linewidth',2);
-plot(e3*1e9+2,VarName5,'g-','Linewidth',1);
-plot(e5*1e9,1.7*ones(1,numel(e5)),'m-','Linewidth',1);
-grid on;
-set(gca,'FontSize',12);
-xlabel('Time (ns)','FontSize',12,'FontWeight','Bold')
-ylabel('Vgs (Volts)','FontSize',12,'FontWeight','Bold')
+plot(e006*1e9,VarName2,'r-','Linewidth',2);
+plot(e2*1e9-2,VarName3,'b-','Linewidth',2);
+plot(e1*1e9+2,VarName1,'k-','Linewidth',2);
+plot(e1*1e9,1.7*ones(1,numel(e1)),'g--','Linewidth',2);
+%grid on;
+set(gca,'FontSize',14);
+xlabel('Time (ns)','FontSize',14,'FontWeight','Bold')
+ylabel('Gate-Source Voltage (Volts)','FontSize',14,'FontWeight','Bold')
 xlim([2160 2240]);
 ylim([-8 3]);
-legend('Phase-A','Phase-B','Phase-C','threshold');
-
+legend({'Phase-A','Phase-B','Phase-C','Threshold'},'Location','best');
 
 %% Top results Vds
 figure;
 hold all;
-plot(e1*1e9,VarName1,'b-','Linewidth',2);
-plot(e008*1e9,VarName2,'r-','Linewidth',2);
-grid on;
-set(gca,'FontSize',12);
-xlabel('Time (ns)','FontSize',12,'FontWeight','Bold')
-ylabel('Phase-C Vds (Volts)','FontSize',12,'FontWeight','Bold')
+plot(e1*1e9,VarName1,'r-','Linewidth',2);
+plot(e008*1e9,VarName2,'b-','Linewidth',2);
+plot(e1*1e9,300*ones(1,numel(e1)),'g--','Linewidth',2);
+%grid on;
+set(gca,'FontSize',14);
+xlabel('Time (ns)','FontSize',14,'FontWeight','Bold')
+ylabel('Drain-Source Voltage (Volts)','FontSize',14,'FontWeight','Bold')
 xlim([-5 60]);
-legend('Bottom','Top');
+legend({'Load Top','Load Bottom','DC Link Voltage'},'Location','best');
 
 %% Top results Vgs
 
 figure;
 hold all;
-plot(e1*1e9-2175,VarName1,'r-','Linewidth',2);
+%plot(e1*1e9-2175,VarName1,'r-','Linewidth',2);
 plot(e006*1e9,VarName2,'b-','Linewidth',2);
 grid on;
 set(gca,'FontSize',12);
 xlabel('Time (ns)','FontSize',12,'FontWeight','Bold')
 ylabel('Phase-C Vgs (Volts)','FontSize',12,'FontWeight','Bold')
-xlim([-50 150]);
+%xlim([-50 250]);
 legend('Bottom','Top');
+
+%% Vgs turn-on and turn-off
+Vgs_turn_on = VarName2(14850:14850+1250);
+Vgs_turn_off = VarName2(19100:19100+1250);
+timeax = 0:0.2:250;
+
+figure;
+hold all;
+plot(timeax,Vgs_turn_on,'b-','Linewidth',2);
+plot(timeax,Vgs_turn_off,'r-','Linewidth',2);
+plot(timeax,6*ones(1,numel(timeax)),'k--','Linewidth',2);
+plot(timeax,-3.7*ones(1,numel(timeax)),'g--','Linewidth',2);
+grid off;
+set(gca,'FontSize',14);
+xlabel('Time (ns)','FontSize',14,'FontWeight','Bold')
+ylabel('Gate-Source Voltage (Volts)','FontSize',14,'FontWeight','Bold')
+%xlim([-50 250]);
+legend({'Turn-on','Turn-off','Positive drive voltage','Negative drive voltage'},'Location','best');
 
 %% Top results Vgs-modify
 
