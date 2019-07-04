c5eab69334b9bad8b827e55d5a664fb1a728b251
mesutto <mesutugur@gmail.com>
2019-07-04 18:13:19 +0300

    Son testin raporu tamamlandı.

--- Prototype/Test/test diaries/2019.06.26/ecce_data_parallel.m
+++ Prototype/Test/test diaries/2019.06.26/ecce_data_parallel.m
@@ -102,60 +102,7 @@ plot(time_yesint_200v_200khz*1e3,Vdc_yesint_200v_200khz-mean(Vdc_yesint_200v_200
 set(gca,'FontSize',14);
 xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
 ylabel('Voltage (Volts)','FontSize',14,'FontWeight','Bold')
-%xlim([1 1.1]);
-legend({'Vdc-No int-200kHz filter','Vdc-Yes int-200kHz filter'},'Location','best');
-title('Parallel - 200V - 200 kHz filtered');
-
-%%
-figure;
-hold all;
-plot(time_noint_200v_725khz*1e3,Vdc_noint_200v_725khz-mean(Vdc_noint_200v_725khz),'b-','Linewidth',1);
-plot(time_yesint_200v_725khz*1e3,Vdc_yesint_200v_725khz-mean(Vdc_yesint_200v_725khz),'r-','Linewidth',1);
-set(gca,'FontSize',14);
-xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
-ylabel('Voltage (Volts)','FontSize',14,'FontWeight','Bold')
-xlim([1 1.1]);
-legend({'Vdc-No int-200kHz filter','Vdc-Yes int-200kHz filter'},'Location','best');
-title('Parallel - 200V - 725 kHz filtered');
-
-
-
-% ///////////
-%% DC current
-figure;
-hold all;
-plot(time_noint_200v_200khz*1e3,Idcm1sh1_noint_200v_200khz-mean(Idcm1sh1_noint_200v_200khz),'r-','Linewidth',1);
-plot(time_noint_200v_200khz*1e3,Idcm2sh3_noint_200v_200khz-mean(Idcm2sh3_noint_200v_200khz),'b-','Linewidth',1);
-%plot(time_yesint_200v_725khz*1e3,Vdc_yesint_200v_725khz,'g-','Linewidth',1);
-%plot(time_yesint_200v_200khz*1e3,Vdc_yesint_200v_200khz,'b-','Linewidth',1);
-set(gca,'FontSize',14);
-xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
-ylabel('Voltage (Volts)','FontSize',14,'FontWeight','Bold')
 %xlim([0 0.1]);
-legend({'Vdc-No int-725kHz filter','Vdc-No int-200kHz filter'},'Location','best');
-title('Parallel - 200V - No interleaving');
-
-%%
-figure;
-hold all;
-plot(time_yesint_200v_725khz*1e3,Vdc_yesint_200v_725khz-mean(Vdc_yesint_200v_725khz),'b-','Linewidth',1);
-plot(time_yesint_200v_200khz*1e3,Vdc_yesint_200v_200khz-mean(Vdc_yesint_200v_200khz),'r-','Linewidth',1);
-set(gca,'FontSize',14);
-xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
-ylabel('Voltage (Volts)','FontSize',14,'FontWeight','Bold')
-xlim([0 0.1]);
-legend({'Vdc-Yes int-725kHz filter','Vdc-Yes int-200kHz filter'},'Location','best');
-title('Parallel - 200V - Yes interleaving');
-
-%%
-figure;
-hold all;
-plot(time_noint_200v_200khz*1e3,Vdc_noint_200v_200khz-mean(Vdc_noint_200v_200khz),'b-','Linewidth',1);
-plot(time_yesint_200v_200khz*1e3,Vdc_yesint_200v_200khz-mean(Vdc_yesint_200v_200khz),'r-','Linewidth',1);
-set(gca,'FontSize',14);
-xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
-ylabel('Voltage (Volts)','FontSize',14,'FontWeight','Bold')
-%xlim([1 1.1]);
 legend({'Vdc-No int-200kHz filter','Vdc-Yes int-200kHz filter'},'Location','best');
 title('Parallel - 200V - 200 kHz filtered');
 
@@ -171,22 +118,163 @@ xlim([1 1.1]);
 legend({'Vdc-No int-200kHz filter','Vdc-Yes int-200kHz filter'},'Location','best');
 title('Parallel - 200V - 725 kHz filtered');
 
-%%
+
+%% DC current - CP
+figure;
+hold all;
+plot(time_noint_200v_200khz*1e3,Idcm1cp1_noint_200v_200khz,'r-','Linewidth',1);
+plot(time_noint_200v_200khz*1e3,Idcm2cp2_noint_200v_200khz,'b-','Linewidth',1);
+set(gca,'FontSize',14);
+xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
+ylabel('Current (Amps)','FontSize',14,'FontWeight','Bold')
+xlim([0 0.1]);
+legend({'Idc-1','Idc-2'},'Location','best');
+title('Parallel - 200V - No interleaving - 200 kHz');
 
 %%
-% %%
-% figure;
-% hold all;
-% %plot(time_noint_200v_725khz*1e3,Vdc_noint_200v_725khz,'b-','Linewidth',1);
-% %plot(time_noint_200v_200khz*1e3,Vdc_noint_200v_200khz,'r-','Linewidth',1);
-% plot(time_noint_200v_725khz*1e3,Idcm1cp1_noint_200v_725khz,'b-','Linewidth',1);
-% plot(time_noint_200v_200khz*1e3,Idcm1cp1_noint_200v_200khz,'r-','Linewidth',1);
-% set(gca,'FontSize',14);
-% xlabel('Time (ms)','FontSize',14,'FontWeight','Bold')
-% %ylabel('DC Link voltage ripple (V)','FontSize',14,'FontWeight','Bold')
-% %xlim([-4.08 -3.97]);
-% %ylim([0.5 1.1]);
-% %legend({'Total','Top module','Bottom module'},'Location','best');
-% %title('Series - 100V - No int - 200 kHz filtered');
-% 
+figure;
+hold all;
+plot(time_yesint_200v_200khz*1e3,Idcm1cp1_yesint_200v_200khz,'r-','Linewidth',1);
+plot(time_yesint_200v_200khz*1e3,Idcm2cp2_yesint_200v_200khz,'b-','Linewidth',1);
+set(gca,'FontSize',14);
+xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
+ylabel('Current (Amps)','FontSize',14,'FontWeight','Bold')
+xlim([0 0.1]);
+legend({'Idc-1','Idc-2'},'Location','best');
+title('Parallel - 200V - Yes interleaving - 200 kHz');
+
+%%
+figure;
+hold all;
+plot(time_noint_200v_200khz*1e3,Idcm1cp1_noint_200v_200khz-Idcm2cp2_noint_200v_200khz,'b-','Linewidth',1);
+plot(time_noint_200v_200khz*1e3,Idcm1cp1_noint_200v_200khz+Idcm2cp2_noint_200v_200khz,'r-','Linewidth',1);
+set(gca,'FontSize',14);
+xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
+ylabel('Current (Amps)','FontSize',14,'FontWeight','Bold')
+xlim([0 0.1]);
+legend({'Difference of currents','Sum of currents'},'Location','best');
+title('Parallel - 200V - No interleaving - 200 kHz');
+
+%%
+figure;
+hold all;
+plot(time_yesint_200v_200khz*1e3,Idcm1cp1_yesint_200v_200khz-Idcm2cp2_yesint_200v_200khz,'b-','Linewidth',1);
+plot(time_yesint_200v_200khz*1e3,Idcm1cp1_yesint_200v_200khz+Idcm2cp2_yesint_200v_200khz,'r-','Linewidth',1);
+set(gca,'FontSize',14);
+xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
+ylabel('Current (Amps)','FontSize',14,'FontWeight','Bold')
+xlim([0 0.1]);
+legend({'Difference of currents','Sum of currents'},'Location','best');
+title('Parallel - 200V - Yes interleaving - 200 kHz');
+
+%%
+figure;
+hold all;
+plot(time_yesint_200v_200khz*1e3,Idcm1cp1_yesint_200v_200khz-Idcm2cp2_yesint_200v_200khz,'b-','Linewidth',1);
+plot(time_noint_200v_200khz*1e3,Idcm1cp1_noint_200v_200khz-Idcm2cp2_noint_200v_200khz,'r-','Linewidth',1);
+set(gca,'FontSize',14);
+xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
+ylabel('Current (Amps)','FontSize',14,'FontWeight','Bold')
+xlim([0 0.1]);
+legend({'Difference of currents - Yes int','Difference of currents - No int'},'Location','best');
+title('Parallel - 200V - 200 kHz');
+
+%%
+figure;
+hold all;
+plot(time_yesint_200v_200khz*1e3,Idcm1cp1_yesint_200v_200khz+Idcm2cp2_yesint_200v_200khz,'b-','Linewidth',1);
+plot(time_noint_200v_200khz*1e3,Idcm1cp1_noint_200v_200khz+Idcm2cp2_noint_200v_200khz,'r-','Linewidth',1);
+set(gca,'FontSize',14);
+xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
+ylabel('Current (Amps)','FontSize',14,'FontWeight','Bold')
+xlim([0 0.1]);
+legend({'Sum of currents - Yes int','Sum of currents - No int'},'Location','best');
+title('Parallel - 200V - 200 kHz');
+
+
+
+%% DC current - SR
+figure;
+hold all;
+plot(time_noint_200v_200khz*1e3,Idcm1sh1_noint_200v_200khz,'r-','Linewidth',1);
+plot(time_noint_200v_200khz*1e3,Idcm2sh3_noint_200v_200khz,'b-','Linewidth',1);
+set(gca,'FontSize',14);
+xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
+ylabel('Current (Amps)','FontSize',14,'FontWeight','Bold')
+xlim([0 0.1]);
+ylim([0 5]);
+legend({'Idc-1','Idc-2'},'Location','best');
+title('Parallel - 200V - No interleaving - 200 kHz');
+
+%%
+figure;
+hold all;
+plot(time_yesint_200v_200khz*1e3,Idcm1sh1_yesint_200v_200khz,'r-','Linewidth',1);
+plot(time_yesint_200v_200khz*1e3,Idcm2sh3_yesint_200v_200khz,'b-','Linewidth',1);
+set(gca,'FontSize',14);
+xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
+ylabel('Current (Amps)','FontSize',14,'FontWeight','Bold')
+xlim([0 0.1]);
+legend({'Idc-1','Idc-2'},'Location','best');
+title('Parallel - 200V - Yes interleaving - 200 kHz');
+
+%%
+figure;
+hold all;
+plot(time_noint_200v_200khz*1e3,Idcm1sh1_noint_200v_200khz-Idcm2sh3_noint_200v_200khz,'b-','Linewidth',1);
+plot(time_noint_200v_200khz*1e3,Idcm1sh1_noint_200v_200khz+Idcm2sh3_noint_200v_200khz,'r-','Linewidth',1);
+set(gca,'FontSize',14);
+xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
+ylabel('Current (Amps)','FontSize',14,'FontWeight','Bold')
+%xlim([0 0.1]);
+legend({'Difference of currents','Sum of currents'},'Location','best');
+title('Parallel - 200V - No interleaving - 200 kHz');
+
+%%
+figure;
+hold all;
+plot(time_yesint_200v_200khz*1e3,Idcm1sh1_yesint_200v_200khz-Idcm2sh3_yesint_200v_200khz,'b-','Linewidth',1);
+plot(time_yesint_200v_200khz*1e3,Idcm1sh1_yesint_200v_200khz+Idcm2sh3_yesint_200v_200khz,'r-','Linewidth',1);
+set(gca,'FontSize',14);
+xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
+ylabel('Current (Amps)','FontSize',14,'FontWeight','Bold')
+%xlim([0 0.1]);
+legend({'Difference of currents','Sum of currents'},'Location','best');
+title('Parallel - 200V - Yes interleaving - 200 kHz');
+
+%%
+figure;
+hold all;
+plot(time_yesint_200v_200khz*1e3,Idcm1sh1_yesint_200v_200khz-Idcm2sh3_yesint_200v_200khz,'b-','Linewidth',1);
+plot(time_noint_200v_200khz*1e3,Idcm1sh1_noint_200v_200khz-Idcm2sh3_noint_200v_200khz,'r-','Linewidth',1);
+set(gca,'FontSize',14);
+xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
+ylabel('Current (Amps)','FontSize',14,'FontWeight','Bold')
+%xlim([0 0.1]);
+legend({'Difference of currents - Yes int','Difference of currents - No int'},'Location','best');
+title('Parallel - 200V - 200 kHz');
+
+%%
+figure;
+hold all;
+plot(time_noint_200v_200khz*1e3,Idcm1sh1_noint_200v_200khz+Idcm2sh3_noint_200v_200khz,'r-','Linewidth',1);
+plot(time_yesint_200v_200khz*1e3,Idcm1sh1_yesint_200v_200khz+Idcm2sh3_yesint_200v_200khz,'b-','Linewidth',1);
+set(gca,'FontSize',14);
+xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
+ylabel('Current (Amps)','FontSize',14,'FontWeight','Bold')
+%xlim([0 0.1]);
+legend({'Sum of currents - No int','Sum of currents - Yes int'},'Location','best');
+title('Parallel - 200V - 200 kHz');
+
+%% etkisi olmadý
+figure;
+hold all;
+plot(time_yesint_200v_725khz*1e3,Idcm1sh1_yesint_200v_725khz-Idcm2sh3_yesint_200v_725khz,'r-','Linewidth',1);
+plot(time_yesint_200v_200khz*1e3,Idcm1sh1_yesint_200v_200khz-Idcm2sh3_yesint_200v_200khz,'b-','Linewidth',1);
+set(gca,'FontSize',14);
+xlabel('Time (ms)','FontSize',14,'FontWeight','Bold');
+ylabel('Current (Amps)','FontSize',14,'FontWeight','Bold')
+xlim([0 0.1]);
+legend({'725 kHz filtered','200 kHz filtered'},'Location','best');
+title('Parallel - 200V - Difference of currents - Yes int');
 
