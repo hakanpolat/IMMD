fd177375c13bc4b36532a06d3dbe1d5297d51180
Furkan <tokgoz.furkan@metu.edu.tr>
2019-09-05 15:20:30 +0300

    paralel iki modüllü simülasyon yaratıldı matlab kodu eklendi

--- Simulation/balancing/balancing_sim_parameters.m
+++ Simulation/balancing/balancing_sim_parameters.m
@@ -74,7 +74,7 @@ Vin = Vdc + Rin*(Pout/Vdc);
 
 
 
-%% RL Load
+%% Series RL Load
 Ts = 1e-7; % sec
 Tfinal = 0.05; % sec
 Ripth = 0.04; % sec
@@ -187,9 +187,122 @@ Pout = P1 + P2;
 Rin = 70;
 Vin = Vdc + Rin*(Pout/Vdc)+3;
 
-%% RL Load with rectifier
+%% Parallel RL Load
 Ts = 1e-7; % sec
 Tfinal = 0.05; % sec
+Ripth = 0.04; % sec
+fsw = 40e3; % Hz
+Vdc = 270; % Volts
+fout = 50; % Hz
+wout = 2*pi*fout; % rad/sec
+m = 3;
+np = 2;
+ns = 1;
+n = ns*np;
+phase = [0 90 0 90];
+Cdc = 15e-6;
+delta = 0; % deg
+
+% calculate nominal values
+Pmodt = 2e3;
+pft = 0.9;
+manom = 0.9;
+Vdcnom = Vdc/ns;
+Vllrmsnom = 0.612*manom*Vdcnom;
+Vlnrms = Vllrmsnom/sqrt(3);
+Smodt = Pmodt/pft;
+Ilinemod = Smodt/(3*Vlnrms);
+Znom = Vlnrms/Ilinemod;
+Rnom = Znom*pft;
+Xnom = sqrt(Znom^2-Rnom^2);
+Lnom = Xnom/(2*pi*fout);
+
+% balanced load
+% Lload_1A = Lnom; % Henries
+% Lload_1B = Lnom; % Henries
+% Lload_1C = Lnom; % Henries
+% Rload_1A = Rnom; % Ohms
+% Rload_1B = Rnom; % Ohms
+% Rload_1C = Rnom; % Ohms
+% Lload_2A = Lnom; % Henries
+% Lload_2B = Lnom; % Henries
+% Lload_2C = Lnom; % Henries
+% Rload_2A = Rnom; % Ohms
+% Rload_2B = Rnom; % Ohms
+% Rload_2C = Rnom; % Ohms
+
+% unbalanced load
+Lload_1A = Lnom*1.20; % Henries
+Lload_1B = Lnom*1.20; % Henries
+Lload_1C = Lnom*1.20; % Henries
+Rload_1A = Rnom*1.20; % Ohms
+Rload_1B = Rnom*1.20; % Ohms
+Rload_1C = Rnom*1.20; % Ohms
+
+Lload_2A = Lnom*1.00; % Henries
+Lload_2B = Lnom*1.00; % Henries
+Lload_2C = Lnom*1.00; % Henries
+Rload_2A = Rnom*1.00; % Ohms
+Rload_2B = Rnom*1.00; % Ohms
+Rload_2C = Rnom*1.00; % Ohms
+
+X1A = 2*pi*fout*Lload_1A; % Ohm
+X1B = 2*pi*fout*Lload_1B; % Ohm
+X1C = 2*pi*fout*Lload_1C; % Ohm
+X2A = 2*pi*fout*Lload_2A; % Ohm
+X2B = 2*pi*fout*Lload_2B; % Ohm
+X2C = 2*pi*fout*Lload_2C; % Ohm
+
+Z1A = sqrt(Rload_1A^2+X1A^2); % Ohm
+Z1B = sqrt(Rload_1B^2+X1B^2); % Ohm
+Z1C = sqrt(Rload_1C^2+X1C^2); % Ohm
+Z2A = sqrt(Rload_2A^2+X2A^2); % Ohm
+Z2B = sqrt(Rload_2B^2+X2B^2); % Ohm
+Z2C = sqrt(Rload_2C^2+X2C^2); % Ohm
+
+pf1A = Rload_1A/Z1A;
+pf1B = Rload_1B/Z1B;
+pf1C = Rload_1C/Z1C;
+pf2A = Rload_2A/Z2A;
+pf2B = Rload_2B/Z2B;
+pf2C = Rload_2C/Z2C;
+
+% No balancing
+ma1 = 0.9;
+ma2 = 0.9;
+
+% Balancing
+% Rload1 = Rload_1A;
+% Rload2 = Rload_2A;
+% pf1 = pf1A;
+% pf2 = pf2A;
+% ratioma = sqrt(Rload1/Rload2)*(pf2/pf1);
+% ma2 = 0.9;
+% ma1 = ma2*ratioma;
+
+% ratio_R = mean([Rload_2A,Rload_2B,Rload_2C])/mean([Rload_1A,Rload_1B,Rload_1C]);
+% ratio_pf = mean([pf1A,pf1B,pf1C])^2/mean([pf2A,pf2B,pf2C])^2;
+% ratio_ma = (ma1/ma2)^2;
+% ratio = ratio_ma*ratio_pf*ratio_R;
+Vdc1 = Vdc;
+Vdc2 = Vdc1;
+V1 = 0.612*ma1*Vdc1/sqrt(3);
+I1 = V1/mean([Z1A,Z1B,Z1C]);
+S1 = 3*V1*I1;
+P1 = S1*mean([pf1A,pf1B,pf1C]);
+V2 = 0.612*ma2*Vdc2/sqrt(3);
+I2 = V2/mean([Z2A,Z2B,Z2C]);
+S2 = 3*V2*I2;
+P2 = S2*mean([pf2A,pf2B,pf2C]);
+
+Pout = P1 + P2;
+
+Rin = 70;
+Vin = Vdc + Rin*(Pout/Vdc)+3;
+
+%% Series RL Load with rectifier
+Ts = 1e-7; % sec
+Tfinal = 0.1; % sec
 Ripth = 0.18; % sec
 fsw = 40e3; % Hz
 Vdc = 540; % Volts
@@ -304,7 +417,134 @@ Pout = P1 + P2;
  Rmid = 4;
  Cdcrec = 4e-3;
 Voutdcrec = Vdc + Rmid*(Pout/Vdc);
-Vacrms=190;
+Vacrms=230;
+% 
+ GridFreq = 50;
+% Vacrms = Voutdcrec*2*pi/(3*sqrt(6));
+ 
+% Voutdcrec = Vacrms*sqrt(2)*sqrt(3);
+% Poutrec = Pmodt;
+% Ioutrec = Poutrec/Voutdcrec;
+% Rloadrec = Voutdcrec/Ioutrec;
+
+%% Parallel RL Load with rectifier
+Ts = 1e-7; % sec
+Tfinal = 0.1; % sec
+Ripth = 0.18; % sec
+fsw = 40e3; % Hz
+Vdc = 270; % Volts
+fout = 50; % Hz
+wout = 2*pi*fout; % rad/sec
+m = 3;
+np = 2;
+ns = 1;
+n = ns*np;
+phase = [0 90 0 90];
+Cdc = 15e-6;
+delta = 0; % deg
+
+% calculate nominal values
+Pmodt = 2e3;
+pft = 0.9;
+manom = 0.9;
+Vdcnom = Vdc/ns;
+Vllrmsnom = 0.612*manom*Vdcnom;
+Vlnrms = Vllrmsnom/sqrt(3);
+Smodt = Pmodt/pft;
+Ilinemod = Smodt/(3*Vlnrms);
+Znom = Vlnrms/Ilinemod;
+Rnom = Znom*pft;
+Xnom = sqrt(Znom^2-Rnom^2);
+Lnom = Xnom/(2*pi*fout);
+
+% balanced load
+% Lload_1A = Lnom; % Henries
+% Lload_1B = Lnom; % Henries
+% Lload_1C = Lnom; % Henries
+% Rload_1A = Rnom; % Ohms
+% Rload_1B = Rnom; % Ohms
+% Rload_1C = Rnom; % Ohms
+% Lload_2A = Lnom; % Henries
+% Lload_2B = Lnom; % Henries
+% Lload_2C = Lnom; % Henries
+% Rload_2A = Rnom; % Ohms
+% Rload_2B = Rnom; % Ohms
+% Rload_2C = Rnom; % Ohms
+
+% unbalanced load
+Lload_1A = Lnom*1.00; % Henries
+Lload_1B = Lnom*1.00; % Henries
+Lload_1C = Lnom*1.00; % Henries
+Rload_1A = Rnom*1.00; % Ohms
+Rload_1B = Rnom*1.00; % Ohms
+Rload_1C = Rnom*1.00; % Ohms
+
+Lload_2A = Lnom*1.00; % Henries
+Lload_2B = Lnom*1.00; % Henries
+Lload_2C = Lnom*1.00; % Henries
+Rload_2A = Rnom*1.00; % Ohms
+Rload_2B = Rnom*1.00; % Ohms
+Rload_2C = Rnom*1.00; % Ohms
+
+X1A = 2*pi*fout*Lload_1A; % Ohm
+X1B = 2*pi*fout*Lload_1B; % Ohm
+X1C = 2*pi*fout*Lload_1C; % Ohm
+X2A = 2*pi*fout*Lload_2A; % Ohm
+X2B = 2*pi*fout*Lload_2B; % Ohm
+X2C = 2*pi*fout*Lload_2C; % Ohm
+
+Z1A = sqrt(Rload_1A^2+X1A^2); % Ohm
+Z1B = sqrt(Rload_1B^2+X1B^2); % Ohm
+Z1C = sqrt(Rload_1C^2+X1C^2); % Ohm
+Z2A = sqrt(Rload_2A^2+X2A^2); % Ohm
+Z2B = sqrt(Rload_2B^2+X2B^2); % Ohm
+Z2C = sqrt(Rload_2C^2+X2C^2); % Ohm
+
+pf1A = Rload_1A/Z1A;
+pf1B = Rload_1B/Z1B;
+pf1C = Rload_1C/Z1C;
+pf2A = Rload_2A/Z2A;
+pf2B = Rload_2B/Z2B;
+pf2C = Rload_2C/Z2C;
+
+% No balancing
+ma1 = 0.9;
+ma2 = 0.9;
+
+% Balancing
+% Rload1 = Rload_1A;
+% Rload2 = Rload_2A;
+% pf1 = pf1A;
+% pf2 = pf2A;
+% ratioma = sqrt(Rload1/Rload2)*(pf2/pf1);
+% ma2 = 0.9;
+% ma1 = ma2*ratioma;
+
+% ratio_R = mean([Rload_2A,Rload_2B,Rload_2C])/mean([Rload_1A,Rload_1B,Rload_1C]);
+% ratio_pf = mean([pf1A,pf1B,pf1C])^2/mean([pf2A,pf2B,pf2C])^2;
+% ratio_ma = (ma1/ma2)^2;
+% ratio = ratio_ma*ratio_pf*ratio_R;
+Vdc1 = Vdc;
+Vdc2 = Vdc1;
+V1 = 0.612*ma1*Vdc1/sqrt(3);
+I1 = V1/mean([Z1A,Z1B,Z1C]);
+S1 = 3*V1*I1;
+P1 = S1*mean([pf1A,pf1B,pf1C]);
+V2 = 0.612*ma2*Vdc2/sqrt(3);
+I2 = V2/mean([Z2A,Z2B,Z2C]);
+S2 = 3*V2*I2;
+P2 = S2*mean([pf2A,pf2B,pf2C]);
+
+Pout = P1 + P2;
+
+
+
+% Pout = Pmodt;
+ Rin = 0;
+ Rmid = 4;
+ Cdcrec = 4e-3;
+Voutdcrec = Vdc + Rmid*(Pout/Vdc);
+Vacrms=140;
 % 
  GridFreq = 50;
 % Vacrms = Voutdcrec*2*pi/(3*sqrt(6));
